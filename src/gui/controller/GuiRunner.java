d3ceee2566bef20b3b3b15fd4d5e32402b5c2a8c
AarooneyFarr <Aarooney.Farr@gmail.com>
2016-11-01 07:52:54 -0600

    Basic setup

---
+++ src/gui/controller/GuiRunner.java
@@ -0,0 +1,11 @@
+package gui.controller;
+
+public class GuiRunner
+{
+	public static void main(String[]  args)
+	{
+		GuiController firstGuiController = new GuiController();
+		firstGuiController.start();
+	}
+			
+}
