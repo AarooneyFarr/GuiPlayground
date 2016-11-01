91b5346d54e8794f85f67426459063deb67a7974
AarooneyFarr <Aarooney.Farr@gmail.com>
2016-11-01 09:27:05 -0600

    changed button layout

--- src/gui/view/GuiFirstPanel.java
+++ src/gui/view/GuiFirstPanel.java
@@ -2,17 +2,67 @@ package gui.view;
 
 import javax.swing.JPanel;
 import gui.controller.GuiController;
-
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.SpringLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class GuiFirstPanel extends JPanel
 {
 	private GuiController baseController;
-	
+	private JButton colorButton;
+	private JButton randomButton;
+	private SpringLayout baseLayout;
+
 	public GuiFirstPanel(GuiController baseController)
 	{
 		super();
 		this.baseController = baseController;
+
+		colorButton = new JButton("color");
+		randomButton = new JButton("random");
+		baseLayout = new SpringLayout();
 		
-		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+
 	}
+
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		this.setBackground(Color.GREEN);
+		this.add(colorButton);
+		this.add(randomButton);
+
+	}
+
+	private void setupLayout()
+	{
+		baseLayout.putConstraint(SpringLayout.NORTH, colorButton, 0, SpringLayout.NORTH, randomButton);
+		baseLayout.putConstraint(SpringLayout.WEST, colorButton, 45, SpringLayout.EAST, randomButton);
+
+	}
+
+	private void setupListeners()
+	{
+		colorButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				setBackground(Color.BLUE);
+			}
+		});
+
+		randomButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				setBackground(Color.RED);
+			}
+		});
+	}
+
 }
